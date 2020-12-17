f297b125d69a667e9340cfe8be6704e72d5bda54
vitortyabe <76071964+vitortyabe@users.noreply.github.com>
2020-12-16 22:58:13 -0300

    Add files via upload

--- Queries/Query4/Query4.sql
+++ Queries/Query4/Query4.sql
@@ -1,6 +1,7 @@
-SELECT 
-IIF(Notas.Nota < 8, "NULL", Alunos.Nome) AS Alunos, Notas.Nota, Alunos.Valor
-FROM Alunos, Notas 
-WHERE Alunos.Valor 
-BETWEEN Notas.Valor_Min AND Notas.Valor_Max 
-ORDER BY Notas.Nota DESC, Alunos.Nome DESC;
+SELECT
+IIF(Notas.Nota < 8, "NULL", Alunos.Nome), Notas.Nota, Alunos.Valor
+FROM Alunos, Notas
+WHERE Alunos.Valor
+BETWEEN Notas.Valor_Min AND Notas.Valor_Max
+ORDER BY Notas.Nota DESC,
+IIF( IIF(Notas.Nota < 8, "NULL", Alunos.Nome) = "NULL", Alunos.Valor, Alunos.Nome) ASC;
