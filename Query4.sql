e13773e5df5167799c47d5b5064643b4d2d6fbc3
vitortyabe <76071964+vitortyabe@users.noreply.github.com>
2020-12-16 22:58:36 -0300

    Delete Query4.sql

--- Query4.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-IIF(Notas.Nota < 8, "NULL", Alunos.Nome), Notas.Nota, Alunos.Valor
-FROM Alunos, Notas
-WHERE Alunos.Valor
-BETWEEN Notas.Valor_Min AND Notas.Valor_Max
-ORDER BY Notas.Nota DESC,
-IIF( IIF(Notas.Nota < 8, "NULL", Alunos.Nome) = "NULL", Alunos.Valor, Alunos.Nome) ASC;
